4e9946d8626eddee07c39bd611b1fd5fa4d3daca
naoto-70gur1 <naoto.toguri@gmail.com>
2024-02-20 23:45:03 +0900

    made some pages

--- src/config/adminlte.php
+++ src/config/adminlte.php
@@ -253,7 +253,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'adminlte',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -312,8 +312,8 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
+            'text'        => 'members',
+            'url'         => 'admin/members',
             'icon'        => 'far fa-fw fa-file',
             'label'       => 4,
             'label_color' => 'success',
